2a78dda46246ba60109e727871721d294116a9c5
nasg2013 <62192753+nasg2013@users.noreply.github.com>
2020-07-08 11:07:54 -0600

    web angular

--- ApiJavaSprintBoot/src/main/java/com/example/repository/InquiryRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.repository;
-
-import com.example.lab.Inquiry;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface InquiryRepository extends JpaRepository<Inquiry,Integer> {
-}
