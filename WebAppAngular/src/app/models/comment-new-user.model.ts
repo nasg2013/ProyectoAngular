d51a52bd98a34397787bc5b3db7c12e6372994fb
nasg2013 <62192753+nasg2013@users.noreply.github.com>
2020-07-09 02:00:35 -0600

    ver noticias

---
+++ WebAppAngular/src/app/models/comment-new-user.model.ts
@@ -0,0 +1,13 @@
+export class CommentNewUserModel{
+
+    public CommentNewUserId:number;
+    public CommentNewId:number;
+    public CreationDate :string;
+    public CreationUserId:number;
+    public Content : string;
+    public ModificationDate :string;
+    public ModificationUser :number;
+    public Status: string;
+    
+
+}
---
+++ WebAppAngular/src/app/models/comment-new.model.ts
@@ -0,0 +1,10 @@
+export class CommentNewModel{
+
+    public CommentNewId:number;
+    public CreationUserId:number;
+    public Content : string;
+    public ModificationDate :string;
+    public ModificationUser :number;
+    public CreationDate :string;
+
+}
